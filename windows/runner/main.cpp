31490b1824c8e540ce9641d905dfe960efeeb616
Chi Hung Wong <wch.klaus@gmail.com>
2024-07-09 08:44:21 +0800

    feat: Adjust window size and position for optimal desktop experience

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -24,21 +24,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
-  // Retrieve the monitor's resolution
   RECT desktop;
   const HWND hDesktop = GetDesktopWindow();
   GetWindowRect(hDesktop, &desktop);
-  int monitorWidth = static_cast<int>((desktop.right - desktop.left) * 0.5);
-  int monitorHeight = static_cast<int>((desktop.bottom - desktop.top) * 0.5);
+  int monitorWidth = static_cast<int>(desktop.right - desktop.left);
+  int monitorHeight = static_cast<int>(desktop.bottom - desktop.top);
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
+  Win32Window::Point origin(0, 0);
   Win32Window::Size size(monitorWidth, monitorHeight);
   if (!window.Create(L"WhatsPie", origin, size)) {
     return EXIT_FAILURE;
   }
-  // Disable window resizing
-  SetWindowLongPtr(window.GetHandle(), GWL_STYLE, WS_OVERLAPPEDWINDOW & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX);
+
+  // Set the window to full-screen mode
+  SetWindowLongPtr(window.GetHandle(), GWL_STYLE, WS_POPUP);
+  ShowWindow(window.GetHandle(), SW_MAXIMIZE);
+
   window.SetQuitOnClose(true);
 
   ::MSG msg;
